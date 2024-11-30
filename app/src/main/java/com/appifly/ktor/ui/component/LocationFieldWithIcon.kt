5eb5b93d425ee6ea56135fc59f3edd2f890898a5
masum <mdmasum.shuvo.cse@gmail.com>
2024-11-30 21:10:39 +0600

    call zilla list

--- app/src/main/java/com/appifly/ktor/ui/component/LocationFieldWithIcon.kt
+++ app/src/main/java/com/appifly/ktor/ui/component/LocationFieldWithIcon.kt
@@ -14,13 +14,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,12 +29,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.appifly.ktor.R
 import com.appifly.ktor.localtion.FindCurrentLocation
 import com.appifly.ktor.localtion.LatAndLong
-import com.appifly.ktor.ui.theme.KtorTheme
 import com.appifly.ktor.ui.theme.Purple40
 import com.appifly.ktor.ui.theme.gray_bg
 import com.appifly.ktor.ui.theme.light_gray
